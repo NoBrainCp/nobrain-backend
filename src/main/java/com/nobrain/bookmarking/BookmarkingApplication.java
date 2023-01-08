a7ba27586683ba84ec3099295d656009e4287268
yuntaeLee <leeyt1201@gmail.com>
2023-01-08 21:32:26 +0900

    initial commit

---
+++ src/main/java/com/nobrain/bookmarking/BookmarkingApplication.java
@@ -0,0 +1,13 @@
+package com.nobrain.bookmarking;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BookmarkingApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(BookmarkingApplication.class, args);
+    }
+
+}
