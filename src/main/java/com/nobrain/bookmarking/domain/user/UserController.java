2c93f1d1d10d0b0dd1710a64cd69cc1e04f0fa6e
yuntaeLee <leeyt1201@gmail.com>
2023-01-13 03:26:39 +0900

    feat: develop user signup controller

---
+++ src/main/java/com/nobrain/bookmarking/domain/user/UserController.java
@@ -0,0 +1,25 @@
+package com.nobrain.bookmarking.domain.user;
+
+import com.nobrain.bookmarking.domain.user.dto.request.UserSignupRequest;
+import com.nobrain.bookmarking.global.response.model.CommonResult;
+import com.nobrain.bookmarking.global.response.service.ResponseService;
+import lombok.RequiredArgsConstructor;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RestController;
+
+import javax.validation.Valid;
+
+@RestController
+@RequiredArgsConstructor
+public class UserController {
+
+    private final UserService userService;
+    private final ResponseService responseService;
+
+    @PostMapping("/signup")
+    public CommonResult signup(@Valid @RequestBody UserSignupRequest dto) {
+        userService.signUp(dto);
+        return responseService.getSuccessResult();
+    }
+}
