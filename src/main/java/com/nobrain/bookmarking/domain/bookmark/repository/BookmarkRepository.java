ac0bbef561a04782fdd3815d8cbc084703451cf7
yuntaeLee <leeyt1201@gmail.com>
2023-01-19 17:24:21 +0900

    Feat: Develop findAll Bookmark from category method

--- src/main/java/com/nobrain/bookmarking/domain/bookmark/repository/BookmarkRepository.java
+++ src/main/java/com/nobrain/bookmarking/domain/bookmark/repository/BookmarkRepository.java
@@ -1,7 +1,12 @@
 package com.nobrain.bookmarking.domain.bookmark.repository;
 
 import com.nobrain.bookmarking.domain.bookmark.entity.Bookmark;
+import com.nobrain.bookmarking.domain.category.entity.Category;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.List;
+
 public interface BookmarkRepository extends JpaRepository<Bookmark, Long> {
+
+    List<Bookmark> findAllByCategory(Category category);
 }
