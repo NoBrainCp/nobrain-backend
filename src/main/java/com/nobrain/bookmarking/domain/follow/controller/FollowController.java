7f545b30563052d68db43c0f2181561ba4359351
yuntaeLee <leeyt1201@gmail.com>
2023-01-20 16:38:32 +0900

    Feat: Find Follower and Following

--- src/main/java/com/nobrain/bookmarking/domain/follow/controller/FollowController.java
+++ src/main/java/com/nobrain/bookmarking/domain/follow/controller/FollowController.java
@@ -1,13 +1,13 @@
 package com.nobrain.bookmarking.domain.follow.controller;
 
+import com.nobrain.bookmarking.domain.follow.dto.FollowResponse;
 import com.nobrain.bookmarking.domain.follow.service.FollowService;
 import com.nobrain.bookmarking.global.response.model.CommonResult;
+import com.nobrain.bookmarking.global.response.model.ListResult;
+import com.nobrain.bookmarking.global.response.model.SingleResult;
 import com.nobrain.bookmarking.global.response.service.ResponseService;
 import lombok.RequiredArgsConstructor;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 @RequiredArgsConstructor
 @RestController
@@ -16,6 +16,21 @@ public class FollowController {
 
     private final FollowService followService;
     private final ResponseService responseService;
+
+    @GetMapping("/{username}/follows")
+    public SingleResult<FollowResponse.FollowCount> getFollowCount(@PathVariable String username) {
+        return responseService.getSingleResult(followService.getFollowCount(username));
+    }
+
+    @GetMapping("/{username}/follower")
+    public ListResult<FollowResponse.Info> getFollowerList(@PathVariable String username) {
+        return responseService.getListResult(followService.getFollowerList(username));
+    }
+
+    @GetMapping("/{username}/following")
+    public ListResult<FollowResponse.Info> getFollowingList(@PathVariable String username) {
+        return responseService.getListResult(followService.getFollowingList(username));
+    }
 
     @PostMapping("/{toUserId}")
     public CommonResult follow(@PathVariable Long toUserId) {
