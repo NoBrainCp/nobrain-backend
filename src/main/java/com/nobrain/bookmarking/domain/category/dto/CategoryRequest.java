c2a0fbcdf89c9a8419b1365f4a19fe53c75a502a
yuntaeLee <leeyt1201@gmail.com>
2023-01-27 20:55:27 +0900

    Feat: Add isPublic, description fields

--- src/main/java/com/nobrain/bookmarking/domain/category/dto/CategoryRequest.java
+++ src/main/java/com/nobrain/bookmarking/domain/category/dto/CategoryRequest.java
@@ -1,5 +1,6 @@
 package com.nobrain.bookmarking.domain.category.dto;
 
+import com.fasterxml.jackson.annotation.JsonProperty;
 import com.nobrain.bookmarking.domain.category.entity.Category;
 import com.nobrain.bookmarking.domain.user.entity.User;
 import lombok.Getter;
@@ -9,11 +10,17 @@ public class CategoryRequest {
     @Getter
     public static class Create {
         private String name;
+        private String description;
+
+        @JsonProperty("isPublic")
+        private boolean isPublic;
         private String username;
 
         public Category toEntity(User user) {
             return Category.builder()
                     .name(this.name)
+                    .description(description)
+                    .isPublic(isPublic)
                     .user(user)
                     .build();
         }
