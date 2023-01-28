bdab433a35720ceaf267d51ef6dd30c94bff1bd0
yuntaeLee <leeyt1201@gmail.com>
2023-01-28 21:24:35 +0900

    Style: Dependency Injection

--- src/main/java/com/nobrain/bookmarking/global/config/WebConfig.java
+++ src/main/java/com/nobrain/bookmarking/global/config/WebConfig.java
@@ -1,21 +1,19 @@
 package com.nobrain.bookmarking.global.config;
 
+import com.nobrain.bookmarking.domain.auth.service.TokenService;
 import com.nobrain.bookmarking.domain.user.resolver.LoginUserIdArgumentResolver;
+import lombok.RequiredArgsConstructor;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.security.core.token.TokenService;
 import org.springframework.web.method.support.HandlerMethodArgumentResolver;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 import java.util.List;
 
+@RequiredArgsConstructor
 @Configuration
 public class WebConfig implements WebMvcConfigurer {
 
     private final TokenService tokenService;
-
-    public WebConfig(final TokenService tokenService) {
-        this.tokenService = tokenService;
-    }
 
     @Override
     public void addArgumentResolvers(final List<HandlerMethodArgumentResolver> resolvers) {
