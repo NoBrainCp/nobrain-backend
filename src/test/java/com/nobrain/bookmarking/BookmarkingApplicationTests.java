feadf6582e08343f4f4b865755aabba1ebca2595
yuntaeLee <leeyt1201@gmail.com>
2023-02-02 22:57:43 +0900

    [#46] refactor: remove test code

--- src/test/java/com/nobrain/bookmarking/BookmarkingApplicationTests.java
+++ src/test/java/com/nobrain/bookmarking/BookmarkingApplicationTests.java
@@ -1,12 +1,10 @@
 package com.nobrain.bookmarking;
 
-import org.junit.jupiter.api.Test;
-
 //@SpringBootTest
 class BookmarkingApplicationTests {
 
-    @Test
-    void test() {
-
-    }
+//    @Test
+//    void test() {
+//
+//    }
 }
