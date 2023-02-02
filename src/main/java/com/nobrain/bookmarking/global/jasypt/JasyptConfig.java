01d9e6fdc7760859ac3ddc4b7acb267130190546
yuntaeLee <leeyt1201@gmail.com>
2023-02-02 19:35:03 +0900

    [#46] feat: apply jasypt

---
+++ src/main/java/com/nobrain/bookmarking/global/jasypt/JasyptConfig.java
@@ -0,0 +1,37 @@
+package com.nobrain.bookmarking.global.jasypt;
+
+import org.jasypt.encryption.StringEncryptor;
+import org.jasypt.encryption.pbe.PooledPBEStringEncryptor;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+public class JasyptConfig {
+
+    @Value("${jasypt.encryptor.password}")
+    private String password;
+
+    @Value("${jasypt.encryptor.algorithm}")
+    private String algorithm;
+
+    @Value("${jasypt.encryptor.pool-size}")
+    private int poolSize;
+
+    @Value("${jasypt.encryptor.output-type}")
+    private String outputType;
+
+    @Value("${jasypt.encryptor.iterations}")
+    private int iterations;
+
+    @Bean(name = "jasyptStringEncryptor")
+    public StringEncryptor stringEncryptor() {
+        PooledPBEStringEncryptor encryptor = new PooledPBEStringEncryptor();
+        encryptor.setPassword(password);
+        encryptor.setAlgorithm(algorithm);
+        encryptor.setPoolSize(poolSize);
+        encryptor.setStringOutputType(outputType);
+        encryptor.setKeyObtentionIterations(iterations);
+        return encryptor;
+    }
+}
