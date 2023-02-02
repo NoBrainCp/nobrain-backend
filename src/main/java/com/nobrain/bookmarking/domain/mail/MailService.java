3100ab14c1544914652a3b4eadf0da5a2f386d7e
yuntaeLee <leeyt1201@gmail.com>
2023-02-02 16:03:22 +0900

    feat: mail send method using thymeleaf

--- src/main/java/com/nobrain/bookmarking/domain/mail/MailService.java
+++ src/main/java/com/nobrain/bookmarking/domain/mail/MailService.java
@@ -1,18 +1,33 @@
 package com.nobrain.bookmarking.domain.mail;
 
+import com.nobrain.bookmarking.global.redis.RedisUtil;
 import lombok.RequiredArgsConstructor;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.mail.SimpleMailMessage;
 import org.springframework.mail.javamail.JavaMailSender;
 import org.springframework.stereotype.Service;
+import org.thymeleaf.TemplateEngine;
+import org.thymeleaf.context.Context;
+import org.thymeleaf.templatemode.TemplateMode;
+import org.thymeleaf.templateresolver.ClassLoaderTemplateResolver;
 
+import javax.mail.MessagingException;
+import javax.mail.internet.MimeMessage;
 import java.util.List;
+import java.util.concurrent.ThreadLocalRandom;
 
 @RequiredArgsConstructor
 @Service
 public class MailService {
     
     private static final String AUTHENTICATION_MAIL_SUBJECT = "Nobrain 인증코드 메일입니다.";
+    private static final long DURATION = 60 * 30L;
+
     private final JavaMailSender mailSender;
+    private final RedisUtil redisUtil;
+
+    @Value("${spring.mail.username}")
+    private String configEmail;
 
     public void sendMailToUsers(List<String> emails, String subject, String text) {
         int size = emails.size();
@@ -40,5 +55,56 @@ public class MailService {
         simpleMailMessage.setText(text);
 
         mailSender.send(simpleMailMessage);
+    }
+
+    public void sendEmail(String toEmail) throws MessagingException {
+        if (redisUtil.existData(toEmail)) {
+            redisUtil.deleteData(toEmail);
+        }
+
+        MimeMessage emailForm = creatEmailForm(toEmail);
+        mailSender.send(emailForm);
+    }
+
+    public Boolean verifyEmailCode(String email, String code) {
+        String findCodeByEmail = redisUtil.getData(email);
+        if (findCodeByEmail == null) {
+            return false;
+        }
+
+        return findCodeByEmail.equals(code);
+    }
+
+    private MimeMessage creatEmailForm(String email) throws MessagingException {
+        String authCode = generatedCode();
+
+        MimeMessage message = mailSender.createMimeMessage();
+        message.addRecipients(MimeMessage.RecipientType.TO, email);
+        message.setSubject(AUTHENTICATION_MAIL_SUBJECT);
+        message.setFrom(configEmail);
+
+        redisUtil.setDataExpire(email, authCode, DURATION);
+        return message;
+    }
+
+    private String setContext(String code) {
+        Context context = new Context();
+        TemplateEngine templateEngine = new TemplateEngine();
+        ClassLoaderTemplateResolver templateResolver = new ClassLoaderTemplateResolver();
+
+        context.setVariable("code", code);
+
+        templateResolver.setPrefix("templates/");
+        templateResolver.setSuffix(".html");
+        templateResolver.setTemplateMode(TemplateMode.HTML);
+        templateResolver.setCacheable(false);
+
+        templateEngine.setTemplateResolver(templateResolver);
+
+        return templateEngine.process("mail", context);
+    }
+
+    private String generatedCode() {
+        return String.valueOf(ThreadLocalRandom.current().nextInt(100000, 1000000));
     }
 }
