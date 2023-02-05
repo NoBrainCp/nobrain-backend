031306446d29beefc9c774dd2bd1c3c3e06a58bb
yuntaeLee <leeyt1201@gmail.com>
2023-02-06 00:28:11 +0900

    [#55] feat: implement mail authentication API and error code

--- src/main/java/com/nobrain/bookmarking/domain/mail/MailController.java
+++ src/main/java/com/nobrain/bookmarking/domain/mail/MailController.java
@@ -1,12 +1,14 @@
 package com.nobrain.bookmarking.domain.mail;
 
+import com.nobrain.bookmarking.domain.mail.dto.MailRequest;
+import com.nobrain.bookmarking.global.response.model.CommonResult;
+import com.nobrain.bookmarking.global.response.service.ResponseService;
 import lombok.RequiredArgsConstructor;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import javax.mail.MessagingException;
+
+import static com.nobrain.bookmarking.global.error.ErrorCode.INVALID_AUTH_CODE;
 
 @RequiredArgsConstructor
 @RestController
@@ -14,9 +16,20 @@ import javax.mail.MessagingException;
 public class MailController {
 
     private final MailService mailService;
+    private final ResponseService responseService;
 
-    @GetMapping("/mail/{mail}")
-    public void sendAuthenticationMail(@PathVariable String mail) throws MessagingException {
-        mailService.sendEmail(mail);
+    @GetMapping("mail/{mail}/authcode")
+    public CommonResult sendAuthenticationMail(@PathVariable String mail) throws MessagingException {
+        mailService.sendEmailForAuthentication(mail);
+        return responseService.getSuccessResult();
+    }
+
+    @PostMapping("mail/{mail}/authcode")
+    public CommonResult sendEmailAndCode(@PathVariable String mail, @RequestBody MailRequest.Authentication requestDto) {
+        if (mailService.verifyEmailCode(mail, requestDto.getCode())) {
+            return responseService.getSuccessResult();
+        }
+
+        return responseService.getFailResult(INVALID_AUTH_CODE.getStatus(), INVALID_AUTH_CODE.getMessage());
     }
 }
