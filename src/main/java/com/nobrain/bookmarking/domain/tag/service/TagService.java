78b8e09df5295e877e66b3ff9ab11bc75bd6c39e
yuntaeLee <leeyt1201@gmail.com>
2023-02-09 02:08:22 +0900

    [#59] feat: tag create, delete logic

--- src/main/java/com/nobrain/bookmarking/domain/tag/service/TagService.java
+++ src/main/java/com/nobrain/bookmarking/domain/tag/service/TagService.java
@@ -1,9 +1,14 @@
 package com.nobrain.bookmarking.domain.tag.service;
 
+import com.nobrain.bookmarking.domain.bookmark_tag.entity.BookmarkTag;
+import com.nobrain.bookmarking.domain.tag.entity.Tag;
 import com.nobrain.bookmarking.domain.tag.repository.TagRepository;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
+
+import java.util.List;
+import java.util.stream.Collectors;
 
 @Transactional(readOnly = true)
 @RequiredArgsConstructor
@@ -12,13 +17,25 @@ public class TagService {
 
     private final TagRepository tagRepository;
 
-//    @Transactional
-//    public void createTags(Bookmark bookmark) {
-//        List<Tag> tags = new CopyOnWriteArrayList<>(bookmark.getTags());
-//        for (Tag tag : tags) {
-//            tag.addBookmark(bookmark);
-//        }
-//
-//        tagRepository.saveAll(bookmark.getTags());
-//    }
+    @Transactional
+    public Tag findOrCreateTags(String tagName) {
+        Tag tag = tagRepository.findByName(tagName)
+                .orElseGet(() -> Tag.builder()
+                        .name(tagName)
+                        .build());
+
+        return tagRepository.save(tag);
+    }
+
+    @Transactional
+    public void deleteAllByBookmarkTags(List<BookmarkTag> bookmarkTags) {
+        List<Tag> removedTags = mapToTags(bookmarkTags);
+        tagRepository.deleteAll(removedTags);
+    }
+
+    private List<Tag> mapToTags(List<BookmarkTag> bookmarkTags) {
+        return bookmarkTags.stream()
+                .map(BookmarkTag::getTag)
+                .collect(Collectors.toList());
+    }
 }
