56cc5ed227a944324f7b5ad2c064601d157f54af
junxtar <aa1870088@naver.com>
2023-02-25 20:17:25 +0900

    [#76] feat: implemented a multi-message transfer function

--- src/main/java/com/nobrain/bookmarking/domain/certification/phone/PhoneController.java
+++ src/main/java/com/nobrain/bookmarking/domain/certification/phone/PhoneController.java
@@ -1,10 +1,13 @@
 package com.nobrain.bookmarking.domain.certification.phone;
 
+import com.nobrain.bookmarking.domain.certification.phone.dto.PhoneRequest;
+import com.nobrain.bookmarking.global.response.model.SingleResult;
+import com.nobrain.bookmarking.global.response.service.ResponseService;
 import lombok.RequiredArgsConstructor;
+import net.nurigo.sdk.message.response.MultipleDetailMessageSentResponse;
 import net.nurigo.sdk.message.response.SingleMessageSentResponse;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
+import retrofit2.http.Path;
 
 @RestController
 @RequiredArgsConstructor
@@ -12,9 +15,18 @@ import org.springframework.web.bind.annotation.RestController;
 public class PhoneController {
 
     private final PhoneService phoneService;
+    private final ResponseService responseService;
 
-    @PostMapping("/send-one")
-    public SingleMessageSentResponse sendOne() {
-        return phoneService.sendSingleMessage();
+    @PostMapping("/phone/{phoneNumber}/authcode")
+    public SingleResult<SingleMessageSentResponse> sendOne(@PathVariable String phoneNumber) {
+        SingleMessageSentResponse response = phoneService.sendSingleMessage(phoneNumber);
+        return responseService.getSingleResult(response);
     }
+
+    @PostMapping("/phone/messages")
+    public SingleResult<MultipleDetailMessageSentResponse> sendMany(@RequestBody PhoneRequest.MultipleMessage dto) {
+        MultipleDetailMessageSentResponse response = phoneService.sendManyMessage(dto);
+        return responseService.getSingleResult(response);
+    }
+
 }
--- src/main/java/com/nobrain/bookmarking/domain/certification/phone/PhoneService.java
+++ src/main/java/com/nobrain/bookmarking/domain/certification/phone/PhoneService.java
@@ -1,18 +1,41 @@
 package com.nobrain.bookmarking.domain.certification.phone;
 
+import com.nobrain.bookmarking.domain.certification.phone.dto.PhoneRequest;
+import com.nobrain.bookmarking.domain.user.repository.UserRepository;
+import com.nobrain.bookmarking.global.redis.RedisUtil;
+import lombok.RequiredArgsConstructor;
 import net.nurigo.sdk.NurigoApp;
+import net.nurigo.sdk.message.exception.NurigoEmptyResponseException;
+import net.nurigo.sdk.message.exception.NurigoMessageNotReceivedException;
+import net.nurigo.sdk.message.exception.NurigoUnknownException;
+import net.nurigo.sdk.message.model.Balance;
 import net.nurigo.sdk.message.model.Message;
+import net.nurigo.sdk.message.model.StorageType;
 import net.nurigo.sdk.message.request.SingleMessageSendingRequest;
+import net.nurigo.sdk.message.response.MultipleDetailMessageSentResponse;
 import net.nurigo.sdk.message.response.SingleMessageSentResponse;
 import net.nurigo.sdk.message.service.DefaultMessageService;
+import org.jetbrains.annotations.NotNull;
 import org.springframework.beans.factory.annotation.Value;
+import org.springframework.core.io.ClassPathResource;
 import org.springframework.stereotype.Service;
+import org.thymeleaf.context.Context;
 
 import javax.annotation.PostConstruct;
+import javax.mail.MessagingException;
+import java.io.File;
+import java.io.IOException;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.concurrent.ThreadLocalRandom;
 
+@RequiredArgsConstructor
 @Service
 public class PhoneService {
 
+    private static final String AUTHENTICATION_MAIL_SUBJECT = "Nobrain 인증코드 메일입니다.";
+    private static final long DURATION = 60 * 30L;
+    private final RedisUtil redisUtil;
     @Value("${COOLSMS.API_KEY}")
     private String API_KEY;
 
@@ -21,7 +44,6 @@ public class PhoneService {
 
     @Value("${COOLSMS.PHONE_NUMBER}")
     private String FROM_PHONE_NUMBER;
-
     private DefaultMessageService messageService;
 
     @PostConstruct
@@ -29,14 +51,83 @@ public class PhoneService {
         this.messageService = NurigoApp.INSTANCE.initialize(API_KEY, API_SECRET_KEY, "https://api.coolsms.co.kr");
     }
 
-    public SingleMessageSentResponse sendSingleMessage() {
+    public void sendPhoneForAuthentication(String toPhoneNumber) {
+        if (redisUtil.existData(toPhoneNumber)) {
+            redisUtil.deleteData(toPhoneNumber);
+        }
+
+        sendSingleMessage(toPhoneNumber);
+    }
+
+    public Boolean verifyPhoneNumberCode(String phoneNumber, String code) {
+        String findCodeByPhoneNumber = redisUtil.getData(phoneNumber);
+        if(findCodeByPhoneNumber == null) {
+            return false;
+        }
+
+        return findCodeByPhoneNumber.equals(code);
+    }
+
+    public SingleMessageSentResponse sendSingleMessage(String phoneNumber) {
+        String authCode = generatedCode();
+        String text = AUTHENTICATION_MAIL_SUBJECT + "\n" + "CODE: " + authCode;
+
+        Message message = createMessage(phoneNumber, text);
+
+        SingleMessageSentResponse response = this.messageService.sendOne(new SingleMessageSendingRequest(message));
+        redisUtil.setDataExpire(phoneNumber, authCode, DURATION);
+        return response;
+    }
+
+    public SingleMessageSentResponse sendMmsByResourcePath(String phoneNumber, String text) throws IOException {
+        ClassPathResource resource = new ClassPathResource("static/sample.png");
+        File file = resource.getFile();
+        String imageId = this.messageService.uploadFile(file, StorageType.MMS, null);
+
+        Message message = createMessage(phoneNumber, text);
+        message.setImageId(imageId);
+
+        return this.messageService.sendOne(new SingleMessageSendingRequest(message));
+    }
+
+    public MultipleDetailMessageSentResponse sendManyMessage(PhoneRequest.MultipleMessage dto) {
+        ArrayList<Message> messageList = new ArrayList<>();
+
+        for (String phoneNumber : dto.getPhoneNumbers()) {
+            messageList.add(createMessage(phoneNumber, dto.getText()));
+        }
+
+        try {
+            MultipleDetailMessageSentResponse response = this.messageService.send(messageList, false, true);
+            return response;
+        } catch (NurigoMessageNotReceivedException e) {
+            throw new RuntimeException(e);
+        } catch (NurigoEmptyResponseException e) {
+            throw new RuntimeException(e);
+        } catch (NurigoUnknownException e) {
+            throw new RuntimeException(e);
+        }
+    }
+
+    public Balance getBalance() {
+        Balance balance = this.messageService.getBalance();
+        return balance;
+    }
+
+    private Message createMessage(String phoneNumber, String text) {
         Message message = new Message();
 
         message.setFrom(FROM_PHONE_NUMBER);
-        message.setTo("01040188963");
-        message.setText("임시 메세지");
+        message.setTo(phoneNumber);
+        message.setText(text);
 
-        SingleMessageSentResponse response = this.messageService.sendOne(new SingleMessageSendingRequest(message));
-        return response;
+        return message;
     }
+
+    private String generatedCode() {
+        return String.valueOf(ThreadLocalRandom.current().nextInt(100000, 1000000));
+    }
+
 }
+
+
