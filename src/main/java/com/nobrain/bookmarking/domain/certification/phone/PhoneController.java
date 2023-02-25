220440f11089783b6a002cb397509b30dd9b097a
junxtar <aa1870088@naver.com>
2023-02-25 21:01:34 +0900

    [#76] feat: implemented a message balance check function.

--- src/main/java/com/nobrain/bookmarking/domain/certification/phone/PhoneController.java
+++ src/main/java/com/nobrain/bookmarking/domain/certification/phone/PhoneController.java
@@ -1,15 +1,20 @@
 package com.nobrain.bookmarking.domain.certification.phone;
 
+import com.nobrain.bookmarking.domain.certification.dto.CertificationRequest;
 import com.nobrain.bookmarking.domain.certification.phone.dto.PhoneRequest;
+import com.nobrain.bookmarking.global.response.model.CommonResult;
 import com.nobrain.bookmarking.global.response.model.SingleResult;
 import com.nobrain.bookmarking.global.response.service.ResponseService;
 import lombok.RequiredArgsConstructor;
+import net.nurigo.sdk.message.model.Balance;
 import net.nurigo.sdk.message.response.MultipleDetailMessageSentResponse;
 import net.nurigo.sdk.message.response.SingleMessageSentResponse;
 import org.springframework.web.bind.annotation.*;
 import retrofit2.http.Path;
 
 import java.io.IOException;
+
+import static com.nobrain.bookmarking.global.error.ErrorCode.INVALID_AUTH_CODE;
 
 @RestController
 @RequiredArgsConstructor
@@ -19,10 +24,20 @@ public class PhoneController {
     private final PhoneService phoneService;
     private final ResponseService responseService;
 
+    @GetMapping("message/balance")
+    public SingleResult<Balance> getBalance(){
+        Balance balance = phoneService.getBalance();
+        return responseService.getSingleResult(balance);
+    }
+
     @PostMapping("/phone/{phoneNumber}/authcode")
-    public SingleResult<SingleMessageSentResponse> sendOne(@PathVariable String phoneNumber) {
-        SingleMessageSentResponse response = phoneService.sendSingleMessage(phoneNumber);
-        return responseService.getSingleResult(response);
+    public CommonResult sendCodeAndLoginId(@PathVariable String phoneNumber, @RequestBody CertificationRequest.Code requestDto) {
+       if(phoneService.verifyPhoneNumberCode(phoneNumber, requestDto.getCode())){
+           phoneService.sendSingleMessage(phoneNumber);
+           return responseService.getSuccessResult();
+       }
+
+       return responseService.getFailResult(INVALID_AUTH_CODE.getStatus(), INVALID_AUTH_CODE.getMessage());
     }
 
     @PostMapping("/phone/messages")
