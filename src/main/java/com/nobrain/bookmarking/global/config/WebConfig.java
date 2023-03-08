d677dd0ae94432ef2934bf7cc28dea4aeb8116ed
yuntaeLee <leeyt1201@gmail.com>
2023-03-08 17:58:22 +0900

    [#93] feat: cors setting using addCorsMappings method

--- src/main/java/com/nobrain/bookmarking/global/config/WebConfig.java
+++ src/main/java/com/nobrain/bookmarking/global/config/WebConfig.java
@@ -5,6 +5,7 @@ import com.nobrain.bookmarking.domain.user.resolver.LoginUserIdArgumentResolver;
 import lombok.RequiredArgsConstructor;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.method.support.HandlerMethodArgumentResolver;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 import java.util.List;
@@ -14,6 +15,13 @@ import java.util.List;
 public class WebConfig implements WebMvcConfigurer {
 
     private final TokenService tokenService;
+
+    @Override
+    public void addCorsMappings(CorsRegistry registry) {
+        registry.addMapping("/**")
+                .allowedOrigins("http://localhost:8080", "http://localhost:8089", "http://localhost:5173")
+                .allowedMethods("GET", "POST", "PUT", "DELETE");
+    }
 
     @Override
     public void addArgumentResolvers(final List<HandlerMethodArgumentResolver> resolvers) {
