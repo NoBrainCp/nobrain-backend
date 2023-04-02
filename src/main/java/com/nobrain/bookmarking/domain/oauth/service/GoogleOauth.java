036bd9157640bc63ed5427d9b5ce79cb0b1d7827
yuntaeLee <leeyt1201@gmail.com>
2023-04-02 14:11:28 +0900

    [#148] style: change package structure

--- src/main/java/com/nobrain/bookmarking/domain/oauth/service/GoogleOauth.java
+++ src/main/java/com/nobrain/bookmarking/domain/oauth/service/GoogleOauth.java
@@ -1,4 +1,4 @@
-package com.nobrain.bookmarking.global.oauth;
+package com.nobrain.bookmarking.domain.oauth.service;
 
 import com.fasterxml.jackson.core.JsonProcessingException;
 import com.fasterxml.jackson.databind.ObjectMapper;
