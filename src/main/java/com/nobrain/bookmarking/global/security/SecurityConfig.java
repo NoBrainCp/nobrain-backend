6a6858e5379a4d7cfe0a4d154fcf194af6959fc7
yuntaeLee <leeyt1201@gmail.com>
2023-04-02 13:56:31 +0900

    [#148] feat: implemented oauth2 google with security

--- src/main/java/com/nobrain/bookmarking/global/security/SecurityConfig.java
+++ src/main/java/com/nobrain/bookmarking/global/security/SecurityConfig.java
@@ -1,6 +1,7 @@
 package com.nobrain.bookmarking.global.security;
 
 import com.nobrain.bookmarking.domain.auth.service.TokenService;
+import com.nobrain.bookmarking.domain.user.service.PrincipalOAuth2DetailsService;
 import lombok.RequiredArgsConstructor;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
@@ -15,6 +16,8 @@ import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.web.SecurityFilterChain;
 import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
 import org.springframework.web.cors.CorsConfiguration;
+import org.springframework.web.cors.CorsConfigurationSource;
+import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
 
 import java.util.List;
 
@@ -24,6 +27,7 @@ import java.util.List;
 public class SecurityConfig {
 
     private final TokenService tokenService;
+    private final PrincipalOAuth2DetailsService principalOAuth2DetailsService;
 
     @Bean
     public PasswordEncoder encoder() {
@@ -39,26 +43,34 @@ public class SecurityConfig {
     protected SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
         http
             .cors()
-                .configurationSource(request -> {
-                    var cors = new CorsConfiguration();
-                    cors.setAllowedOrigins(List.of("http://localhost:5173"));
-                    cors.setAllowedMethods(List.of("GET", "POST", "PUT", "DELETE", "OPTIONS"));
-                    cors.setAllowedHeaders(List.of("*"));
-                    return cors;
-                })
-                .and()
+            .and()
             .httpBasic().disable()
             .csrf().disable()
             .sessionManagement().sessionCreationPolicy(SessionCreationPolicy.STATELESS) // 세션 생성 안함.
             .and()
                 .authorizeHttpRequests()
-                .antMatchers("/*/sign-in", "/*/sign-in/**/" , "/*/sign-up", "/*/sign-up/**" , "/social/**", "/**").permitAll()
+                .antMatchers("/*/sign-in", "/*/sign-in/**/" , "/*/sign-up", "/*/sign-up/**", "/social/**", "/oauth2/**", "/login/oauth2/code/google", "/user/oauth/password/**").permitAll()
 //                .anyRequest().hasRole("USER")
             .and()
             .addFilterBefore(new JwtAuthenticationFilter(tokenService),
                     UsernamePasswordAuthenticationFilter.class); // JwtAuthenticationFilter를 UsernamePasswordAuthenticationFilter 전에 넣는다.
 
+        http
+            .oauth2Login()
+            .userInfoEndpoint()
+            .userService(principalOAuth2DetailsService);
+
         return http.build();
+    }
+
+    @Bean
+    public CorsConfigurationSource configurationSource() {
+        CorsConfiguration configuration = new CorsConfiguration();
+        configuration.setAllowedOrigins(List.of("http://localhost:5173"));
+        configuration.setAllowedMethods(List.of("GET", "POST", "PUT", "DELETE"));
+        UrlBasedCorsConfigurationSource source = new UrlBasedCorsConfigurationSource();
+        source.registerCorsConfiguration("/auth/google/callback", configuration);
+        return source;
     }
 
     @Bean
