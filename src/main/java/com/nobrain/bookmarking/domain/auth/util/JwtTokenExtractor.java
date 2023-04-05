e18eeee4d7116a54fe851aea65b17bc478eaabf1
yoon <leeyt1201@gmail.com>
2023-04-06 08:09:26 +0900

    [#60] refactor: change method of util class to static

--- src/main/java/com/nobrain/bookmarking/domain/auth/util/JwtTokenExtractor.java
+++ src/main/java/com/nobrain/bookmarking/domain/auth/util/JwtTokenExtractor.java
@@ -2,21 +2,29 @@ package com.nobrain.bookmarking.domain.auth.util;
 
 import com.nobrain.bookmarking.domain.auth.exception.TokenInvalidException;
 import com.nobrain.bookmarking.domain.auth.exception.TokenNotExistsException;
+import org.springframework.http.HttpHeaders;
 import org.springframework.stereotype.Component;
+
+import javax.servlet.http.HttpServletRequest;
 
 @Component
 public class JwtTokenExtractor {
 
-    public String extract(String authorizationHeader, String tokenType) {
+    private static final String TOKEN_TYPE = "Bearer";
+    private static final int TOKEN_TYPE_INDEX = 0;
+    private static final int TOKEN_INDEX = 1;
+
+    public static String extract(HttpServletRequest request) {
+        String authorizationHeader = request.getHeader(HttpHeaders.AUTHORIZATION);
         if (authorizationHeader == null) {
             throw new TokenNotExistsException("Access Token");
         }
 
         String[] splitHeader = authorizationHeader.split(" ");
-        if (splitHeader.length != 2 || !splitHeader[0].equalsIgnoreCase(tokenType)) {
+        if (splitHeader.length != 2 || !splitHeader[TOKEN_TYPE_INDEX].equalsIgnoreCase(TOKEN_TYPE)) {
             throw new TokenInvalidException(authorizationHeader);
         }
 
-        return splitHeader[1];
+        return splitHeader[TOKEN_INDEX];
     }
 }
