710b51b96459f613aaebb7f2b3d13918102bf0c1
yoon <leeyt1201@gmail.com>
2023-04-06 13:31:01 +0900

    [#160] feat: implemented hasAccessToken method and change extract logic

--- src/main/java/com/nobrain/bookmarking/domain/auth/util/JwtTokenExtractor.java
+++ src/main/java/com/nobrain/bookmarking/domain/auth/util/JwtTokenExtractor.java
@@ -1,30 +1,40 @@
 package com.nobrain.bookmarking.domain.auth.util;
 
-import com.nobrain.bookmarking.domain.auth.exception.TokenInvalidException;
-import com.nobrain.bookmarking.domain.auth.exception.TokenNotExistsException;
+import com.nobrain.bookmarking.domain.auth.exception.AuthorizationHeaderInvalidException;
 import org.springframework.http.HttpHeaders;
 import org.springframework.stereotype.Component;
 
 import javax.servlet.http.HttpServletRequest;
+import java.util.Enumeration;
 
 @Component
 public class JwtTokenExtractor {
 
     private static final String TOKEN_TYPE = "Bearer";
-    private static final int TOKEN_TYPE_INDEX = 0;
     private static final int TOKEN_INDEX = 1;
+    private static final String BLANK = " ";
 
     public static String extract(HttpServletRequest request) {
-        String authorizationHeader = request.getHeader(HttpHeaders.AUTHORIZATION);
-        if (authorizationHeader == null) {
-            throw new TokenNotExistsException("Access Token");
+        Enumeration<String> headers = request.getHeaders(HttpHeaders.AUTHORIZATION);
+        while (headers.hasMoreElements()) {
+            String value = headers.nextElement();
+            if (value.toLowerCase().startsWith(TOKEN_TYPE.toLowerCase())) {
+                return value.split(BLANK)[TOKEN_INDEX];
+            }
         }
 
-        String[] splitHeader = authorizationHeader.split(" ");
-        if (splitHeader.length != 2 || !splitHeader[TOKEN_TYPE_INDEX].equalsIgnoreCase(TOKEN_TYPE)) {
-            throw new TokenInvalidException(authorizationHeader);
+        throw new AuthorizationHeaderInvalidException();
+    }
+
+    public static boolean hasAccessToken(HttpServletRequest request) {
+        Enumeration<String> headers = request.getHeaders(HttpHeaders.AUTHORIZATION);
+        while (headers.hasMoreElements()) {
+            String value = headers.nextElement();
+            if (value.toLowerCase().startsWith(TOKEN_TYPE.toLowerCase())) {
+                return true;
+            }
         }
 
-        return splitHeader[TOKEN_INDEX];
+        return false;
     }
 }
