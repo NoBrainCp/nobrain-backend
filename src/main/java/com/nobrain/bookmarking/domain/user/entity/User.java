1e1e974973a86718d00cc717df993895289dd195
yoon <leeyt1201@gmail.com>
2023-04-06 19:42:54 +0900

    [#151] chore: remove spring security library

--- src/main/java/com/nobrain/bookmarking/domain/user/entity/User.java
+++ src/main/java/com/nobrain/bookmarking/domain/user/entity/User.java
@@ -6,16 +6,11 @@ import lombok.AccessLevel;
 import lombok.Builder;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
-import org.springframework.security.core.GrantedAuthority;
-import org.springframework.security.core.authority.SimpleGrantedAuthority;
-import org.springframework.security.core.userdetails.UserDetails;
 
 import javax.persistence.*;
 import java.time.LocalDate;
 import java.util.ArrayList;
-import java.util.Collection;
 import java.util.List;
-import java.util.stream.Collectors;
 
 import static javax.persistence.CascadeType.REMOVE;
 
@@ -23,7 +18,7 @@ import static javax.persistence.CascadeType.REMOVE;
 @NoArgsConstructor(access = AccessLevel.PROTECTED)
 @Table(name = "users")
 @Entity
-public class User extends BaseTimeEntity implements UserDetails {
+public class User extends BaseTimeEntity {
 
     @Id @GeneratedValue(strategy = GenerationType.IDENTITY)
     @Column(name = "user_id")
@@ -72,37 +67,5 @@ public class User extends BaseTimeEntity implements UserDetails {
 
     public void changeProfileImage(String profileImage) {
         this.profileImage = profileImage;
-    }
-
-    @Override
-    public Collection<? extends GrantedAuthority> getAuthorities() {
-        return this.roles.stream()
-                .map(SimpleGrantedAuthority::new)
-                .collect(Collectors.toList());
-    }
-
-    @Override
-    public String getUsername() {
-        return String.valueOf(id);
-    }
-
-    @Override
-    public boolean isAccountNonExpired() {
-        return true;
-    }
-
-    @Override
-    public boolean isAccountNonLocked() {
-        return true;
-    }
-
-    @Override
-    public boolean isCredentialsNonExpired() {
-        return true;
-    }
-
-    @Override
-    public boolean isEnabled() {
-        return true;
     }
 }
