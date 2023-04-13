d69af1cc5900484b0b3d20a0e591e20c20a72c5d
yoon <leeyt1201@gmail.com>
2023-04-13 14:37:34 +0900

    [#170] feat: validate category request dto

--- src/main/java/com/nobrain/bookmarking/domain/category/dto/CategoryRequest.java
+++ src/main/java/com/nobrain/bookmarking/domain/category/dto/CategoryRequest.java
@@ -5,11 +5,20 @@ import com.nobrain.bookmarking.domain.category.entity.Category;
 import com.nobrain.bookmarking.domain.user.entity.User;
 import lombok.Getter;
 
+import javax.validation.constraints.NotBlank;
+import javax.validation.constraints.NotEmpty;
+import javax.validation.constraints.Size;
+
 public class CategoryRequest {
 
     @Getter
     public static class Info {
+
+        @NotBlank(message = "카테고리 이름은 필수 항목입니다.")
+        @Size(max = 20, message = "이름의 최대 길이는 10글자 입니다.")
         private String name;
+
+        @Size(max = 40, message = "설명의 최대 길이는 30글자 입니다.")
         private String description;
 
         @JsonProperty("isPublic")
