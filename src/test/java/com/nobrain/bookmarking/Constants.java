8b051532699f7065a8ece9103b847d0161a9fd88
yoon <leeyt1201@gmail.com>
2023-05-04 15:56:39 +0900

    [#178] test: user test constants

---
+++ src/test/java/com/nobrain/bookmarking/Constants.java
@@ -0,0 +1,13 @@
+package com.nobrain.bookmarking;
+
+public class Constants {
+
+    private Constants() {}
+
+    public static final Long USER_ID = 1L;
+    public static final String USERNAME = "test";
+    public static final String EMAIL = "test@test.com";
+    public static final String PASSWORD = "testPassword";
+    public static final String PASSWORD_CHECK = "testPassword";
+    public static final String PASSWORD_CHECK_NOT_SAME = "notSameTestPassword";
+}
