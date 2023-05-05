4af85239f7fb9be35c2021e690692893e3c05d7e
yoon <leeyt1201@gmail.com>
2023-05-05 19:01:30 +0900

    [#183] test: implemented redis test code

--- src/test/java/com/nobrain/bookmarking/global/redis/RedisUtilTest.java
+++ src/test/java/com/nobrain/bookmarking/global/redis/RedisUtilTest.java
@@ -1,23 +1,30 @@
 package com.nobrain.bookmarking.global.redis;
 
-//@SpringBootTest
+import org.assertj.core.api.Assertions;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.ActiveProfiles;
+
+@SpringBootTest
+@ActiveProfiles("test")
 class RedisUtilTest {
 
-//    @Autowired
-//    private RedisUtil redisUtil;
-//
-//    @Test
-//    public void redisTest() throws Exception {
-//        //given
-//        String email = "test@test.com";
-//        String code = "test1234";
-//
-//        //when
-//        redisUtil.setDataExpire(email, code, 60 * 60L);
-//
-//        //then
-//        Assertions.assertThat(redisUtil.existData(email)).isTrue();
-//        Assertions.assertThat(redisUtil.existData("fail@fail.com")).isFalse();
-//        Assertions.assertThat(redisUtil.getData(email)).isEqualTo(code);
-//    }
+    @Autowired
+    private RedisUtil redisUtil;
+
+    @Test
+    public void redisTest() throws Exception {
+        //given
+        String email = "test@test.com";
+        String code = "test1234";
+
+        //when
+        redisUtil.setDataExpire(email, code, 60 * 60L);
+
+        //then
+        Assertions.assertThat(redisUtil.existData(email)).isTrue();
+        Assertions.assertThat(redisUtil.existData("fail@fail.com")).isFalse();
+        Assertions.assertThat(redisUtil.getData(email)).isEqualTo(code);
+    }
 }
