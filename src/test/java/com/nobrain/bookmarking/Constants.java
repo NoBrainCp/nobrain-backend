dd62e7be513f2158c87dcc3a6b9ee48a3b71dd50
yoon <leeyt1201@gmail.com>
2023-05-05 18:32:27 +0900

    [#178] test: added user profile image constants

--- src/test/java/com/nobrain/bookmarking/Constants.java
+++ src/test/java/com/nobrain/bookmarking/Constants.java
@@ -4,10 +4,19 @@ public class Constants {
 
     private Constants() {}
 
+    // user
     public static final Long USER_ID = 1L;
     public static final String USERNAME = "test";
+    public static final String NEW_USERNAME = "newName";
     public static final String EMAIL = "test@test.com";
     public static final String PASSWORD = "testPassword";
+    public static final String PASSWORD_NOT_SAME = "notSamePassword";
     public static final String PASSWORD_CHECK = "testPassword";
     public static final String PASSWORD_CHECK_NOT_SAME = "notSameTestPassword";
+    public static final String CHANGE_PASSWORD = "changePassword";
+    public static final String CHANGE_PASSWORD_NOT_SAME = "notSameChangePassword";
+    public static final String PROFILE_IMG = "profileImage";
+    public static final String CHANGE_PROFILE_IMG_NAME = "changeProfileImage";
+    public static final String CHANGE_PROFILE_IMG_PATH = "changeProfileImage.png";
+    public static final String PROFILE_IMG_CONTENT_TYPE_PNG = "image/png";
 }
