5a3bbe723600e54a6f845ac375dfc04e644038db
yoon <leeyt1201@gmail.com>
2023-05-05 19:29:08 +0900

    [#184] test: initial user sign controller test

---
+++ src/test/java/com/nobrain/bookmarking/domain/user/controller/UserSignControllerTest.java
@@ -0,0 +1,12 @@
+package com.nobrain.bookmarking.domain.user.controller;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class UserSignControllerTest {
+
+    @Test
+    void signUp() {
+    }
+}
