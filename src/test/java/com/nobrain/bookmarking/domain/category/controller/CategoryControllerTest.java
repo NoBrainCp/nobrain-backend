68f245efdd5c5d265238958223eeb7a6ae05f61c
yoon <leeyt1201@gmail.com>
2023-05-19 17:56:27 +0900

    [#196] test: implemented the api success test code to get category is public

--- src/test/java/com/nobrain/bookmarking/domain/category/controller/CategoryControllerTest.java
+++ src/test/java/com/nobrain/bookmarking/domain/category/controller/CategoryControllerTest.java
@@ -9,6 +9,7 @@ import org.apache.http.HttpHeaders;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
+import org.springframework.restdocs.payload.JsonFieldType;
 import org.springframework.test.web.servlet.ResultActions;
 
 import javax.servlet.http.HttpServletRequest;
@@ -16,14 +17,14 @@ import java.util.ArrayList;
 import java.util.List;
 
 import static com.nobrain.bookmarking.Constants.*;
-import static org.mockito.ArgumentMatchers.any;
-import static org.mockito.ArgumentMatchers.anyString;
+import static org.mockito.ArgumentMatchers.*;
 import static org.mockito.BDDMockito.given;
 import static org.mockito.Mockito.verify;
 import static org.springframework.restdocs.mockmvc.RestDocumentationRequestBuilders.get;
 import static org.springframework.restdocs.payload.PayloadDocumentation.*;
 import static org.springframework.restdocs.request.RequestDocumentation.parameterWithName;
 import static org.springframework.restdocs.request.RequestDocumentation.pathParameters;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.jsonPath;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 
 class CategoryControllerTest extends RestDocsTestSupport {
@@ -88,10 +89,10 @@ class CategoryControllerTest extends RestDocsTestSupport {
                                         parameterWithName("username").description("유저 이름")
                                 ),
                                 responseFields(beneathPath("data").withSubsectionId("data"),
-                                        fieldWithPath("id").description("카테고리 아이디"),
-                                        fieldWithPath("name").description("카테고리 이름"),
-                                        fieldWithPath("description").description("카테고리 설명"),
-                                        fieldWithPath("public").description("카테고리 공개 여부")
+                                        fieldWithPath("id").type(JsonFieldType.NUMBER).description("카테고리 아이디"),
+                                        fieldWithPath("name").type(JsonFieldType.STRING).description("카테고리 이름"),
+                                        fieldWithPath("description").type(JsonFieldType.STRING).description("카테고리 설명"),
+                                        fieldWithPath("public").type(JsonFieldType.BOOLEAN).description("카테고리 공개 여부")
                                 )
                         )
                 );
@@ -125,11 +126,11 @@ class CategoryControllerTest extends RestDocsTestSupport {
                                 parameterWithName("username").description("유저 이름")
                         ),
                         responseFields(beneathPath("list[]").withSubsectionId("list"),
-                                fieldWithPath("id").description("카테고리 아이디"),
-                                fieldWithPath("name").description("카테고리 이름"),
-                                fieldWithPath("description").description("카테고리 설명"),
-                                fieldWithPath("public").description("카테고리 공개 여부"),
-                                fieldWithPath("count").description("카테고리의 북마크 개수")
+                                fieldWithPath("id").type(JsonFieldType.NUMBER).description("카테고리 아이디"),
+                                fieldWithPath("name").type(JsonFieldType.STRING).description("카테고리 이름"),
+                                fieldWithPath("description").type(JsonFieldType.STRING).description("카테고리 설명"),
+                                fieldWithPath("public").type(JsonFieldType.BOOLEAN).description("카테고리 공개 여부"),
+                                fieldWithPath("count").type(JsonFieldType.NUMBER).description("카테고리의 북마크 개수")
                         )
                 ));
 
@@ -137,11 +138,69 @@ class CategoryControllerTest extends RestDocsTestSupport {
     }
 
     @Test
-    void getCategoryByBookmarkId() {
+    @DisplayName("북마크를 통한 카테고리 조회 - 성공")
+    void getCategoryByBookmarkId() throws Exception {
+        // given
+        given(categoryService.getCategoryByBookmarkId(anyLong()))
+                .willReturn(CategoryResponse.Info.builder()
+                        .id(category.getId())
+                        .name(category.getName())
+                        .description(category.getDescription())
+                        .isPublic(category.isPublic())
+                        .count(CATEGORY_BOOKMARK_COUNT)
+                        .build());
+
+        // when
+        ResultActions result = mockMvc.perform(
+                get(BASE_URL + "/categories/bookmarks/{bookmarkId}", BOOKMARK_ID)
+                        .header(HttpHeaders.AUTHORIZATION, AUTHORIZATION_HEADER));
+
+        // then
+        result.andExpect(status().isOk())
+                .andDo(restDocs.document(
+                        pathParameters(
+                                parameterWithName("bookmarkId").description("북마크 아이디")
+                        ),
+                        responseFields(beneathPath("data").withSubsectionId("data"),
+                                fieldWithPath("id").type(JsonFieldType.NUMBER).description("카테고리 아이디"),
+                                fieldWithPath("name").type(JsonFieldType.STRING).description("카테고리 이름"),
+                                fieldWithPath("description").type(JsonFieldType.STRING).description("카테고리 설명"),
+                                fieldWithPath("public").type(JsonFieldType.BOOLEAN).description("카테고리 공개 여부"),
+                                fieldWithPath("count").type(JsonFieldType.NUMBER).description("카테고리의 북마크 개수")
+                        )
+                ));
+
+        verify(categoryService).getCategoryByBookmarkId(BOOKMARK_ID);
     }
 
     @Test
-    void getCategoryIsPublic() {
+    @DisplayName("카테고리의 공개 여부 조회 - 성공")
+    void getCategoryIsPublic() throws Exception {
+        // given
+        given(categoryService.getCategoryIsPublic(any(), anyString()))
+                .willReturn(category.isPublic());
+
+        // when
+        ResultActions result = mockMvc.perform(
+                get(BASE_URL + "/categories/{categoryName}/public", category.getName())
+                        .header(HttpHeaders.AUTHORIZATION, AUTHORIZATION_HEADER));
+
+        // then
+        result.andExpect(status().isOk())
+                .andExpect(jsonPath("data").value(category.isPublic()))
+                .andDo(restDocs.document(
+                        pathParameters(
+                                parameterWithName("categoryName").description("카테고리 이름")
+                        ),
+                        responseFields(
+                                fieldWithPath("success").type(JsonFieldType.BOOLEAN).description("응답 성공 여부"),
+                                fieldWithPath("code").type(JsonFieldType.NUMBER).description("응답 코드"),
+                                fieldWithPath("message").type(JsonFieldType.STRING).description("응답 메시지"),
+                                fieldWithPath("data").type(JsonFieldType.BOOLEAN).description("카테고리 공개 여부")
+                        )
+                ));
+
+        verify(categoryService).getCategoryIsPublic(payload, category.getName());
     }
 
     @Test
